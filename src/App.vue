0279d137b169444168f70e1aaf640b8e49fbd71c
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-05-09 15:33:03 +0300

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import { RouterLink, RouterView } from "vue-router";
+import AppHeader from "./cmps/AppHeader.vue";
+import AppFooter from "./cmps/AppFooter.vue";
+
+export default {
+  components: {
+    AppFooter,
+    AppHeader
+  }
+}
+</script>
+
+<template>
+  <div class="main-app">
+    <AppHeader />
+    <RouterView />
+    <AppFooter />
+  </div>
+</template>
+
+<style>
+
+</style>
