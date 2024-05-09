d7cc6b089710c82a80f0ebe17dbd9c9c20b79c5a
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-05-09 17:19:44 +0300

    Add ocntactdetails page cmp

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../pages/HomePage.vue'
 import ContactIndex from '../pages/ContactIndex.vue'
+import ContactDetails from '../pages/ContactDetails.vue'
 
 const routerOptions = {
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const routerOptions = {
       path: '/contact',
       name: 'contact',
       component: ContactIndex
+    },
+    {
+      path: '/contact/:id',
+      name: 'details',
+      component: ContactDetails
     }
   ]
 }
