431fd39f0dba4a0ecec25d45b1d1174f9a0eb995
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-05-09 18:14:44 +0300

    Add back button and arrange code for readability

--- src/pages/ContactDetails.vue
+++ src/pages/ContactDetails.vue
@@ -1,10 +1,16 @@
 <template>
-<section class="contact-details" v-if="selectedContact">
-    <img :src="`https://robohash.org/${selectedContact.name}?set=set3`" :alt="`${selectedContact.name} picture`" />
-<h1>{{selectedContact.name}}</h1>
-<h2>Email: </h2><p>{{selectedContact.email}}</p>
-<h2>Phone: </h2><p>{{selectedContact.phone}}</p>
-</section>
+  <section class="contact-details" v-if="selectedContact">
+    <RouterLink to="/contact"><button>Back</button></RouterLink>
+    <img
+      :src="`https://robohash.org/${selectedContact.name}?set=set3`"
+      :alt="`${selectedContact.name} picture`"
+    />
+    <h1>{{ selectedContact.name }}</h1>
+    <h2>Email:</h2>
+    <p>{{ selectedContact.email }}</p>
+    <h2>Phone:</h2>
+    <p>{{ selectedContact.phone }}</p>
+  </section>
 </template>
 
 <script>
@@ -17,7 +23,9 @@ export default {
   },
   async created() {
     try {
-      this.selectedContact = await contactService.getContactById(this.$route.params.id);
+      this.selectedContact = await contactService.getContactById(
+        this.$route.params.id
+      );
     } catch (err) {
       console.log(err);
       throw err;
