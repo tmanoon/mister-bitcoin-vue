b20abcdcc0847c462af1e2b6754c56be4d13e06b
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-05-09 17:57:13 +0300

    Add basics to contact details and remove extra comma in router js file

--- src/pages/ContactDetails.vue
+++ src/pages/ContactDetails.vue
@@ -1,13 +1,30 @@
 <template>
-  
+<section class="contact-details" v-if="selectedContact">
+    <img :src="`https://robohash.org/${selectedContact.name}?set=set3`" :alt="`${selectedContact.name} picture`" />
+<h1>{{selectedContact.name}}</h1>
+<h2>Email: </h2><p>{{selectedContact.email}}</p>
+<h2>Phone: </h2><p>{{selectedContact.phone}}</p>
+</section>
 </template>
 
 <script>
+import { contactService } from "../services/contact.service.js";
 export default {
-
-}
+  data() {
+    return {
+      selectedContact: {},
+    };
+  },
+  async created() {
+    try {
+      this.selectedContact = await contactService.getContactById(this.$route.params.id);
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  },
+};
 </script>
 
 <style>
-
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -20,12 +20,7 @@ const routerOptions = {
       path: '/contact/:id',
       name: 'details',
       component: ContactDetails
-<<<<<<< HEAD
     },
-
-=======
-    }
->>>>>>> ac7b1aeda4a60fa01a29d81abddef26cac762297
   ]
 }
 
