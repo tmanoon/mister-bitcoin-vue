c6e9ba78e7ee631026717a9127fe63b111384742
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-05-09 16:53:35 +0300

    Add basic homepage

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,13 +1,30 @@
 <template>
-  <div>hello</div>
+  <section v-if="user" class=home-page>
+    <h1>Hello, {{user.name}}</h1>
+    <p>{{user.rate}}</p>
+  </section>
 </template>
 
 <script>
+import { userService } from "../services/user.service.js";
+import { bitcoinService } from "../services/bitcoin.service.js";
 export default {
-
-}
+  data() {
+    return {
+      user: {},
+    };
+  },
+  async created() {
+    try {
+      this.user = userService.getUser();
+      this.user.rate = await bitcoinService.getRate(this.user.balance);
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  },
+};
 </script>
 
 <style>
-
 </style>
