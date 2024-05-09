e87cc7214f86eab520cdd301f83a7e0afb5d8e34
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-05-09 16:51:03 +0300

    Add routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from '../pages/HomePage.vue'
 
-const router = createRouter({
+const routerOptions = {
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomePage
+    },
+    {
+      path: '/about',
+      name: 'about',
 
+    }
   ]
-})
+}
+
+const router = createRouter(routerOptions)
 
 export default router
