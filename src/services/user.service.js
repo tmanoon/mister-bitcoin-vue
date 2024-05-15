3b95671bec13e452aa38a46e84b0eeb1ac3e8d38
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-05-15 17:00:57 +0300

    Add functions to service

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,13 +1,34 @@
+import { utilService } from "./util.service"
+
 const user = {
     name: "Pooki Pikpooki",
     balance: 100,
     transactions: []
 }
 
+const CONTACT_DB = 'contact_db'
+
 function getUser() {
     return user
 }
+function signup(username) {
+        const cachedUser = utilService.loadFromStorage(CONTACT_DB)
+        if(cachedUser && cachedUser.username === username) return cachedUser
+        const user = _getEmptyUser()
+        user.username = username
+        utilService.saveToStorage(CONTACT_DB, user)
+        return user
+}
+
+function _getEmptyUser() {
+    return {
+        username: '',
+        balance: 100,
+        transactions: []
+    }
+}
 
 export const userService = {
-    getUser
+    getUser,
+    signup
 }
